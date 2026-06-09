ca07759b1691a72a94f1b8ba5ba31f815b47273c
Anja-Isabella Schulz <anja-isabella.schulz@gmx.de>
2026-06-09 13:45:48 +0200

    feat: implement getPokemon function to fetch data from PokeAPI

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+let pokemonStorage = [];
 
+async function getPokemon() {
+    if (pokemonStorage.length > 0) {
+        return pokemonStorage;
+    }
+
+    for (let i = 1; i <= 30; i++) {
+        let url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        let response = await fetch(url);
+        let data = await response.json();
+
+        pokemonStorage.push({
+            id: data.id,
+            name: data.name,
+            image: data.sprites.other["official-artwork"].front_default
+        });
+    }
+
+    return pokemonStorage;
+}
